d70e90988276183d279e337ce5cef38b5bc132eb
wanderanimrod <wanderanimrod@gmail.com>
2014-07-27 16:48:55 +0300

    Showing world map. Random colors.

--- map.js
+++ map.js
@@ -6,21 +6,24 @@ svg = d3.select("#map").append("svg")
     .attr("height", height);
 
 projection = d3.geo.mercator()
-    .center([0.0, 0.0])
-    .scale(100)
-    .translate([width/2, height/2]);
+    .center([0.0, 10.0])
+    .scale(200)
+    .translate([width / 2, height / 2]);
 
 color = d3.scale.quantize().range(["rgb(237,248,233)", "rgb(186,228,179)",
-            "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"
-            ]);
+    "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"
+]);
 
 path = d3.geo.path().projection(projection);
 
 d3.json("world.json", function(json) {
 
-        svg.selectAll("path")
-                .data(json.features)
+    svg.selectAll("path")
+        .data(json.features)
         .enter()
-                .append("path")
-                .attr("d", path);
+        .append("path")
+        .attr("d", path)
+        .attr("fill", function() {
+            return color(Math.random());
+        })
 });
