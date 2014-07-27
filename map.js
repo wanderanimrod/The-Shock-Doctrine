b09dade4d46aa0cffe56a39f067e328a1b486f2e
wanderanimrod <wanderanimrod@gmail.com>
2014-07-27 17:39:24 +0300

    Adds dot to map at 0,0

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ svg = d3.select("#map").append("svg")
     .attr("height", height);
 
 projection = d3.geo.mercator()
-    .center([0.0, 10.0])
+    .center([0.0, 0.0])
     .scale(200)
     .translate([width / 2, height / 2]);
 
@@ -24,6 +24,14 @@ d3.json("world.json", function(json) {
         .append("path")
         .attr("d", path)
         .attr("fill", "none")
-        .attr("stroke", "rgb(0,172,0)")
+        .attr("stroke", "rgb(50,50,192)")
         .attr("stroke-width", "0.9px")
 });
+
+var center = projection([0, 0]);
+
+svg.append("circle")
+    .attr("cx", center[0])
+    .attr("cy", center[1])
+    .attr("r", 5)
+    .attr("fill", "red");
