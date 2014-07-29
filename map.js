456c46f898bcf3b9626a6aba864801bfebc8c94d
wanderanimrod <wanderanimrod@gmail.com>
2014-07-29 20:30:54 +0300

    A projector-friendly color scheme

--- map.js
+++ map.js
@@ -1,14 +1,16 @@
-var width = 1500,
+var width = "100%",
     height = 800;
 
 svg = d3.select("#map").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("style", "background-color: #0E0E0E");
 
-projection = d3.geo.mercator()
+
+projection = d3.geo.equirectangular()
     .center([0.0, 0.0])
     .scale(200)
-    .translate([width / 2, height / 2]);
+    .translate([700, height / 2]);
 
 color = d3.scale.quantize().range(["rgb(237,248,233)", "rgb(186,228,179)",
     "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"
@@ -18,14 +20,14 @@ path = d3.geo.path().projection(projection);
 
 d3.json("world.json", function(json) {
 
-    svg.selectAll("path")
+    svg.selectAll("path.feature")
         .data(json.features)
         .enter()
         .append("path")
         .attr("d", path)
-        .attr("fill", "none")
-        .attr("stroke", "rgb(50,50,192)")
-        .attr("stroke-width", "0.9px")
+        .attr("fill", "#838383")
+        .attr("stroke", "#0E0E0E")
+        .attr("stroke-width", "0.1px")
 });
 
 d3.json("events.json", function(json) {
@@ -56,7 +58,9 @@ function plot(event) {
         .attr("cx", center[0])
         .attr("cy", center[1])
         .attr("r", 8)
-        .attr("fill", "red");
+        .attr("fill", "#C4C4C4")
+        .attr("stroke", "#FFFFFF")
+        .attr("stroke-width", 0.8);
 }
 
 var terminationEvent = {type: "END"};
